18fe0f403fba8e6be20e0e8cf667a8f976278b7e
Jesper <jeppenp12@gmail.com>
2023-05-15 21:57:20 +0200

    yes

--- firebase/db.js
+++ firebase/db.js
@@ -2,12 +2,12 @@ import { firebase } from "@firebase/app"
 import "@firebase/firestore"
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: "oUYxakS97kUHnOwopW73kw7KeVtiXC5To2y4cizJ",
+    authDomain: "mtg-commander-aaeaf.firebaseapp.com",
+    databaseURL: "https://mtg-commander-aaeaf.firebaseio.com",
+    projectId: "mtg-commander-aaeaf",
+    storageBucket: "mtg-commander-aaeaf.appspot.com",
+    messagingSenderId: "447477765016"
 })
 
 const db = firebaseApp.firestore()
